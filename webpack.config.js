6b63a6baafb2b2b08d139e9a3ef5442e9594d4da
HareeshBhittam <hareesh@reldyn.co>
2023-10-15 11:59:43 +0530

    practice styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
 
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
   return webpackMerge(
     {
       mode,
       output: {
-        filename: "bundle.js"
+        filename: "bundle.js",
       },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
     modeConfig(mode)
   );
